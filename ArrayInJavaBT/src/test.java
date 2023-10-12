8fb8fd833d3a2bae2491e2d4e1627852506f961b
lelong199896 <lelong199896@gmail.com>
2023-10-12 15:59:20 +0700

    ClassAndObjectTH

---
+++ ArrayInJavaBT/src/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public void static f() {} main(String[] args) {
+
+            int a = 100;
+            m(a);
+            System.out.println(a);
+        }
+
+        static void m(int x) {
+            x = 200;
+        }
+
+}
