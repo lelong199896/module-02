d63c56b832a03a4027bee0f54b71af5df34cb9c4
lelong199896 <lelong199896@gmail.com>
2023-10-30 14:39:20 +0700

    IOTextFile

--- Debug/src/Triangle/Triangle.java
+++ Debug/src/Triangle/Triangle.java
@@ -11,12 +11,15 @@ public class Triangle {
         int b = scanner.nextInt();
         System.out.print("Enter the temporary edge: ");
         int c = scanner.nextInt();
-        if (a + b > c && a + c > b && b + c > a) {
-            System.out.println(a + ", " + b + ", " + c + " are the three sides of the triangle");
-        } else if (a < 0 || b < 0 || c < 0) {
-            System.out.println(a + ", " + b + ", " + c + " is not three sides of the triangle");
-        } else {
-            System.out.println(a + ", " + b + ", " + c + " is not three sides of the triangle");
+        try {
+            if (a + b > c && a + c > b && b + c > a) {
+                System.out.println(a + ", " + b + ", " + c + " are the three sides of the triangle");
+            } else if (a < 0 || b < 0 || c < 0) {
+                System.out.println(a + ", " + b + ", " + c + " is not three sides of the triangle");
+            } else {
+                System.out.println(a + ", " + b + ", " + c + " is not three sides of the triangle");
+            }
+        } catch (Exception e) {
         }
     }
 }
