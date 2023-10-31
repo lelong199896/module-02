186c96d2c960c7fb50005c658e29eea6016b5d74
lelong199896 <lelong199896@gmail.com>
2023-10-31 10:06:36 +0700

    IOBinaryFile

---
+++ IOBinary/IOBinaryTH/src/BinaryFile/Main.java
@@ -0,0 +1,52 @@
+package BinaryFile;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void writeDataToFile(String path, List<Student> students) {
+        try {
+            FileOutputStream fos = new FileOutputStream(path);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(students);
+            oos.close();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Student> readDataFromFile(String path) {
+        List<Student> students = new ArrayList<>();
+        try {
+            FileInputStream fis = new FileInputStream(path);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            students = (List<Student>) ois.readObject();
+            fis.close();
+            ois.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return students;
+    }
+
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Vũ Kiều Anh", "Hà Nội"));
+        students.add(new Student(2, "Nguyễn Minh Quân", "Hà Nội"));
+        students.add(new Student(3, "Đặng Huy Hoà", "Đà Nẵng"));
+        students.add(new Student(4, "Nguyễn Khánh Tùng", "Hà Nội"));
+        students.add(new Student(5, "Nguyễn Khắc Nhật", "Hà Nội"));
+
+        writeDataToFile("D:\\codegym\\module-02\\IOBinary\\IOBinaryTH\\src\\BinaryFile\\student.txt", students);
+
+        List<Student> studentDataFromFile = readDataFromFile("D:\\codegym\\module-02\\IOBinary\\IOBinaryTH\\src\\BinaryFile\\student.txt");
+        for (Student s: studentDataFromFile) {
+            System.out.println(s);
+        }
+    }
+}
